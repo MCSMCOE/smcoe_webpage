49c2b084ec9e41650b28fab6c258ee61b6787386
MEDIA CELL SMCOE <122856792+MCSMCOE@users.noreply.github.com>
2023-11-03 14:24:23 +0000

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include("conn.php")
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
+  <meta charset="utf-8"> 
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>Stella Mary's College of Engineering</title>
