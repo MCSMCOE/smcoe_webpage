fda653288ba27d6d0113771c844e3171419c464b
MEDIA CELL SMCOE <122856792+MCSMCOE@users.noreply.github.com>
2023-12-01 06:09:30 +0000

    update

---
+++ dept.sh
@@ -0,0 +1,11 @@
+scp aids.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/aids.php
+scp civil.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/civil.php
+scp cse.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/cse.php
+scp ece.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/ece.php
+scp eee.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/eee.php
+scp mba.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/mba.php
+scp mech.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/mech.php
+scp mecse.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/mecse.php
+scp meise.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/meise.php
+scp sh.php root@109.205.183.103:/home/campusstack/deployment/config/phpmariadb/stellamaryscoe.edu.in/php_files_dir/sh.php
+
