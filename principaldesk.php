e1331b60498710b5832cc589624052957020218c
MEDIA CELL SMCOE <122856792+MCSMCOE@users.noreply.github.com>
2024-03-19 18:43:25 +0000

    update

--- principaldesk.php
+++ principaldesk.php
@@ -202,11 +202,8 @@ include("conn.php")
           <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
             <img src="https://webdocs.pages.dev/assets/img/team/D.jpg" class="img-fluid" alt="">
           </div>
-<<<<<<< HEAD
-          <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
-=======
           <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
->>>>>>> 9136cf3 (update)
+
             <span class="h3 d-inline-block">
               <b class="font-weight-bold">Dr.J.Jenix Rino</b>
               <small class="h6 text-secondary">M.E.,M.B.A.,Ph.D</small>
@@ -214,7 +211,7 @@ include("conn.php")
     
             <p class=" col-md-6 col-lg-12 p-6 fst-italic text-justify" style="line-height:1.5;"  >
               <i class="bx bxs-quote-alt-left quote-icon-left text-secondary h4 mr-6 "></i>
-              I am delighted to welcome you to Stella Mary’s College of Engineering.
+              I am delighted to welcome you to <strong>Stella Mary’s College of Engineering.</Strong>
 Stella Mary’s College of Engineering is one of the best institutes in Kanyakumari District, Tamilnadu.  It is a decade-old institute with well-experienced and qualified faculties. It has world-class infrastructure, lab facilities, and spacious indoor and outdoor sporting facilities. Our core aspiration is to get established as a center of technical excellence. Developing the student community technically strong, with professional ethics, moral values, leadership skills,  and social responsibility are our priorities. 
 	As you explore our website, I hope you are able to find all the information you are looking for, and more, about this vibrant learning community that is socially engaged as well. If you are a prospective student or a potential colleague and have any specific queries or concerns, do not hesitate to contact us.  Our motto is ‘If you walk you can reach, To reach you have to walk’, and I do hope that you have reason to partner with us in this endeavour, in whatever way you can.
               <i class="bx bxs-quote-alt-right quote-icon-right text-secondary h4 ml-3"></i>
@@ -244,11 +241,8 @@ Stella Mary’s College of Engineering is one of the best institutes in Kanyakum
           <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
             <img src="https://webdocs.pages.dev/assets/img/faculty/1278.png" class="img-fluid" alt="">
           </div>
-<<<<<<< HEAD
-          <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
-=======
           <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
->>>>>>> 9136cf3 (update)
+
             <span class="h3 d-inline-block">
               <b class="font-weight-bold">Dr.R.SURESH PREMIL KUMAR</b>
               <small class="h6 text-secondary">M.E.,Ph.D</small>
@@ -284,11 +278,7 @@ Stella Mary’s College of Engineering is one of the best institutes in Kanyakum
           <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
             <img src="https://webdocs.pages.dev/assets/img/faculty/1131.png" class="img-fluid" alt="">
           </div>
-<<<<<<< HEAD
-          <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
-=======
           <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
->>>>>>> 9136cf3 (update)
             <span class="h3 d-inline-block">
               <b class="font-weight-bold">Dr.R.K.Madhumathi</b>
               <small class="h6 text-secondary">M.E, Ph.D</small>
@@ -324,19 +314,147 @@ Stella Mary’s College of Engineering is one of the best institutes in Kanyakum
           <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
             <img src="https://webdocs.pages.dev/assets/img/faculty/1294.png" class="img-fluid" alt="">
           </div>
-<<<<<<< HEAD
-          <div class="col-lg-7 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
-=======
+
+
           <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
->>>>>>> 9136cf3 (update)
+
             <span class="h3 d-inline-block">
               <b class="font-weight-bold">Mrs.Melba Heni</b>
-              <small class="h6 text-secondary">M.B.A.,M.Phil</small>
+              <small class="h6 text-secondary">M.B.A.,</small>
             </span>
     
             <p class="p-8 fst-italic text-justify" style="line-height: 3;"  >
               <i class="bx bxs-quote-alt-left quote-icon-left text-secondary h4 mr-6"></i>
               Welcome to the <strong>Financial Services</strong> Hub at Stella Mary's College of Engineering! We're your partners in navigating financial aspects, ensuring a seamless journey throughout your education. From tuition guidance to financial aid, our team is here to support your goals. Explore our resources and connect with us for personalized assistance. Let's pave the way for your academic success together!
+              <i class="bx bxs-quote-alt-right quote-icon-right text-secondary h4 ml-3"></i>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section class="breadcrumbs">
+      <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Talent Acquisition Head's Desk</h2>
+        </div>
+
+      </div>
+    </section>
+    <section class="principaldesk" data-aos="fade-in">
+      <div class="container">
+       
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about shadow p-4">
+      <div class="container" data-aos="fade-up">
+        <div class="row align-items-start">
+          <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
+            <img src="https://webdocs.pages.dev/assets/img/faculty/1026.png" class="img-fluid" alt="">
+          </div>
+
+
+          <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
+
+            <span class="h3 d-inline-block">
+              <b class="font-weight-bold">Dr.Pon.Partheeban</b>
+              <small class="h6 text-secondary">M.E, Ph.D</small>
+            </span>
+    
+            <p class="p-8 fst-italic text-justify" style="line-height: 3;"  >
+              <i class="bx bxs-quote-alt-left quote-icon-left text-secondary h4 mr-6"></i>
+              Welcome to Stella Mary's College of Engineering <strong>Talent Acquisition Department</Strong>
+
+As the Head of Talent Acquisition, I extend a warm welcome to all prospective candidates. Here at Stella Mary's, we believe in nurturing talent that not only excels academically but also embodies our core values of innovation, integrity, and excellence.
+
+We are dedicated to identifying individuals who are not just skilled, but who also demonstrate a passion for engineering and a commitment to making a positive impact in their chosen field.
+
+Join us in shaping the future of engineering education and research. Explore the opportunities available and embark on a journey of growth and discovery with us at Stella Mary's College of Engineering.
+              <i class="bx bxs-quote-alt-right quote-icon-right text-secondary h4 ml-3"></i>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section class="breadcrumbs">
+      <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Quality Auditor's Desk</h2>
+        </div>
+
+      </div>
+    </section>
+    <section class="principaldesk" data-aos="fade-in">
+      <div class="container">
+       
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about shadow p-4">
+      <div class="container" data-aos="fade-up">
+        <div class="row align-items-start">
+          <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
+            <img src="https://webdocs.pages.dev/assets/img/faculty/1054.png" class="img-fluid" alt="">
+          </div>
+
+
+          <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
+
+            <span class="h3 d-inline-block">
+              <b class="font-weight-bold">Dr.K.Ezhil Vignesh</b>
+              <small class="h6 text-secondary">M.E, Ph.D</small>
+            </span>
+    
+            <p class="p-8 fst-italic text-justify" style="line-height: 3;"  >
+              <i class="bx bxs-quote-alt-left quote-icon-left text-secondary h4 mr-6"></i>
+              Welcome to Stella Mary's College of Engineering's <strong>Quality Assurance Department</Strong>, overseen by our Institutional Quality Assurance Cell (IQAC)!
+
+As the Quality Auditor, it's my privilege to ensure that our institution maintains the highest standards of academic excellence and operational efficiency. Through rigorous assessment and continuous improvement initiatives, we are committed to enhancing the quality of education and research offered at Stella Mary's.
+
+Our IQAC serves as the cornerstone of our commitment to quality enhancement, driving initiatives that foster innovation, accountability, and student-centric learning experiences. We invite you to explore our quality assurance framework and join us in our journey towards excellence.
+
+Together, let's uphold the values of integrity, transparency, and academic rigor that define Stella Mary's College of Engineering.
+              <i class="bx bxs-quote-alt-right quote-icon-right text-secondary h4 ml-3"></i>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    <section class="breadcrumbs">
+      <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Public Relations Officer's Desk</h2>
+        </div>
+
+      </div>
+    </section>
+    <section class="principaldesk" data-aos="fade-in">
+      <div class="container">
+       
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about shadow p-4">
+      <div class="container" data-aos="fade-up">
+        <div class="row align-items-start">
+          <div class="col-lg-3 order-1 order-lg-6" data-aos="fade-right" data-aos-delay="100">
+            <img src="https://webdocs.pages.dev/assets/img/faculty/1094.png" class="img-fluid" alt="">
+          </div>
+
+
+          <div class="col-md-9 pt-4 pt-lg-0 order-2 order-lg-1 justify-content-xl-start">
+
+            <span class="h3 d-inline-block">
+              <b class="font-weight-bold">Mrs.Arokia Latha</b>
+              <small class="h6 text-secondary">M.A.,M.Phil</small>
+            </span>
+    
+            <p class="p-8 fst-italic text-justify" style="line-height: 3;"  >
+              <i class="bx bxs-quote-alt-left quote-icon-left text-secondary h4 mr-6"></i>
+              Welcome to Stella Mary's College of Engineering's <strong>Public Relations Department</Strong>
+
+As the Public Relations Officer, it's my pleasure to welcome you to our vibrant community. At Stella Mary's, we are dedicated to fostering meaningful connections and sharing the inspiring stories that shape our institution's identity.
+
+Through strategic communication initiatives, we aim to highlight the achievements, events, and innovations that make Stella Mary's a dynamic hub of learning and growth. Whether you're a prospective student, alumni, industry partner, or member of the wider community, we invite you to engage with us and be part of our journey.
+
+Explore our website to discover the latest news, upcoming events, and opportunities to connect with us. Together, let's build bridges, forge partnerships, and create a brighter future for Stella Mary's College of Engineering.
               <i class="bx bxs-quote-alt-right quote-icon-right text-secondary h4 ml-3"></i>
             </p>
           </div>
