4c4c9db8dd03f5ebb8ae519c867719e4e53caf8a
MEDIA CELL SMCOE <122856792+MCSMCOE@users.noreply.github.com>
2024-03-21 13:31:20 +0000

    update

---
+++ footer.php
@@ -0,0 +1,88 @@
+<footer id="footer">
+    <div class="container">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+  
+            <!--<div class="col-lg-2 col-md-6 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+              </ul>
+            </div>-->
+  
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>QUICK LINKS 1</h4>
+              <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">NIRF</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">ANTI RAGGING CELL</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">GRIEVANCE REDRESSAL CELL</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">INTERNAL COMPLIANCE CELL</a></li>
+                <li><i class="bx bx-chevron-right "></i> <a href="mandatorydisclosure.php">MANDATORY DISCLOSURE</a></li>
+              </ul>
+            </div>
+  
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>QUICK LINKS 2</h4>
+              <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">ONLINE ADMISSION</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="https://webdocs.pages.dev/assets/docs/iqac/naac/NAAC%20CERTIFICATION%20-%20CYCLE%201.pdf">NAAC CERTIFICATE</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="https://webdocs.pages.dev/assets/docs/iqac/naac/NAAC%20SSR%20REPORT-CYCLE%201.pdf">NAAC SSR</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="https://css.aicte-india.org/login">AICTE Centralised Support System</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="OnlinePaymentPrivacyPolicy.php">Online Payment - Privacy Policy</a></li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-6 footer-links">
+              <h4>Downloads</h4>
+              <ul>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Student Undertaking form</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="https://webdocs.pages.dev/assets/docs/cgptc/PlacementUndertaking.pdf">Placement Undertaking form</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="#">Faculty Undertaking form</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="https://webdocs.pages.dev/assets/docs/r&d/Proposal-Format-SM-SMS.docx">Stella Mary's Seed Money (Doc)</a></li>
+                <li><i class="bx bx-chevron-right"></i> <a href="https://webdocs.pages.dev/assets/docs/r&d/Proposal-Format-SM-SMS.pdf">Stella Mary's Seed Money (Pdf)</a></li>
+              </ul>
+            </div>
+  
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>Stella Mary's College of Engineering</strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/vesperr-free-bootstrap-template/ -->
+            Designed & Maintained by <a href="mediacell.php">SMCE Media Cell</a>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+            <a href="index.php" class="scrollto">Home</a>
+            <a href="" class="scrollto">About</a>
+            <a href="#">Privacy Policy</a>
+            <a href="">Terms of Use</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="https://webdocs.pages.dev/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="https://webdocs.pages.dev/assets/vendor/aos/aos.js"></script>
+  <script src="https://webdocs.pages.dev/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://webdocs.pages.dev/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="https://webdocs.pages.dev/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="https://webdocs.pages.dev/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="https://webdocs.pages.dev/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="https://webdocs.pages.dev/assets/js/main.js"></script>
---
+++ header.php
@@ -0,0 +1,104 @@
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+    <div class="logo">
+    <!--<h1><a href="index.html">SMCE</a></h1>-->
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <a href="index.php"><img src="https://webdocs.pages.dev/assets/img/LOGO%20NEW1.png" alt="" class="img-fluid" style="max-width: 100%; height: auto;"></a>
+</div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.php">Home</a></li>
+          <li class="dropdown"><a href="#"><span>About Us</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li><a href="Management.php">Management</a></li>
+              <li><a href="principaldesk.php">Admin Desk</a></li>
+              <li><a href="oc.php">Organisation Chart</a></li>
+              <li><a href="iqac.php">IQAC</a></li>
+              <li><a href="strategicplan.php">Strategic Plan</a></li>
+              <li><a href="codeofconduct.php">Code of Conduct</a></li>
+              <li><a href="mandatorydisclosure.php">Mandatory Disclosure</a></li>
+              <li><a href="facilities.php">Features</a></li>
+            </ul>
+          <li class="dropdown"><a href="#"><span>Academics</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li class="dropdown"><a href="#more-services" href="#"><span>Department - Under Graduate</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="civil.php">Civil Engineering</a></li>
+                  <li><a href="cse.php">Computer Science and Engineering</a></li>
+                  <li><a href="ece.php">Electronics and Communication Engineering</a></li>
+                  <li><a href="eee.php">Electrical and Electronics Engineering</a></li>
+                  <li><a href="mech.php">Mechanical Engineering</a></li>
+                  <li><a href="aids.php">Artificial Intelligence and Data Science</a></li>
+                  <li><a href="sh.php">Science and Humanities</a></li>
+                </ul>
+              <li class="dropdown"><a href="#more-services" href="#"><span>Department - Post Graduate</span> <i class="bi bi-chevron-right"></i></a>
+                  <ul>
+                    <li><a href="mecse.php">M.E Comuputer Science and Engineering</a></li>
+                    <li><a href="meise.php">M.E Industrial Safety Engineering</a></li>
+                    <li class="dropdown"><a href="mba.php"><span>Master of Business Administration</span> <i class="bi bi-chevron-right"></i></a>
+                      <ul>
+                        <li><a href="mba.php">Operations Management</a></li>
+                        <li><a href="mba.php">Human Resources Management</a></li>
+                        <li><a href="mba.php">Finncial Management</a></li>
+                        <li><a href="mba.php">Business Analytics Management</a></li>
+                        <li><a href="mba.php">Marketing Management</a></li>
+                        <li><a href="mba.php">Logistics & Supply Chain Management</a></li>
+                      </ul>  
+                  </ul>  
+              <li><a href="placement_home.php">Placement</a></li>
+              <!--<li><a href="#">Academic Time-Table</a></li>
+              <li><a href="#">Academic Calender</a></li>
+              <li><a href="#">Admission Criteria</a></li>-->
+              <li><a href="https://camps.stellamaryscoe.edu.in/CAMPS/CommonJSP/signin.jsp">CAMPS LOGIN</a></li>
+            </ul>
+            <li class="dropdown"><a href="#"><span>Research</span> <i class="bi bi-chevron-right"></i></a>
+              <ul>
+                <li><a href="research_codeofethics.php">Team & Code of Ethics</a></li>
+                <li><a href="research_publications.php">Publications</a></li>
+                <li><a href="research_seedmoney.php">Stella Mary's Seed Money</a></li>
+                <li><a href="ipr.php">Intellectual Property Rights</a></li>
+                <li class="dropdown"><a href="research_seedmoney.php">Industry Collaboration<i class="bi bi-chevron-right"></i></a>
+                <ul>
+                <li><a href="research_mou.php">Memorandum of Understanding</a></li>
+                <li><a href="https://webdocs.pages.dev/assets/docs/r&d/Starup-Policy.pdf">Startup Policy</a></li>
+                </ul>
+              </ul>
+              
+              <li class="dropdown"><a href="#"><span>Life at SMCE</span> <i class="bi bi-chevron-right"></i></a>
+              <ul>
+              <li class="dropdown"><a href="#more-services" href="#"><span>Co-curricular</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                <li><a href="cfi.php">Centre For Innovation</a></li>
+                <li><a href="culturalclub.php">Cultural Club</a></li>
+                <li><a href="Sports.php">Sports</a></li>
+                <li><a href="lc.php">Literary Club</a></li>
+                </ul>
+              <li class="dropdown"><a href="#more-services" href="#"><span>Extra Curricular</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                <li><a href="nss.php">National Service Scheme</a></li>
+                <li><a href="yrc.php">Youth Red Cross</a></li>  
+                </ul>  
+                <li><a href="facilities.php">Infrastructure/Gallery</a></li>
+                <li><a href="icc.php">Internal Complaint Cell</a></li>
+                <li><a href="research_codeofethics.php">Research and Development Cell</a></li>
+                <li><a href="placement_home.php">Career Guidance and Placement Training Cell</a></li>
+                <li><a href="mediacell.php">Media Cell</a></li>
+                <li><a href="grievance.php">Grievance Redressal Cell</a></li>
+                <li><a href="ar.php">Anti Ragging Cell</a></li>
+                <li><a href="edc.php">Entrepreneurship Development Cell</a></li>
+                <li><a href="https://www.infomirrorscloud.com/smce.webopac/Imt_frm_User_Login.aspx">Library</a></li>
+              <li><a href="https://nptel.ac.in">NPTEL</a></li>
+              <li><a href="https://delnet.in">DELNET</a></li>
+              <li><a href="alumni.php">Alumni Association</a></li>
+              <li><a href="https://swayam.gov.in">SWYAM - MOOC</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="mandatorydisclosure.php">Mandatory Disclosure</a></li>
+          <li><a class="getstarted scrollto" href="https://camps.stellamaryscoe.edu.in/CAMPS/applications/application_register.jsp">Admission</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
